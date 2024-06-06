8f6e2d664f6bc84954bb20d40109d130eece9b0a
edg524aln <162599753+edg524aln@users.noreply.github.com>
2024-06-07 00:50:34 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,4 +4,17 @@ $(function() {
         $('.modal').toggleClass('modal-open');
     });
 
+    $('.question').on('click', function(){
+        $('.answer').slideUp(500);
+
+        var findElm = $(this).next('.answer');
+        
+        if ($(this).hasClass('close')) {
+            $(this).removeClass('close');
+        } else {
+            $('.close').removeClass('close');
+            $(this).addClass('close');
+            $(findElm).slideDown(500);
+        }
+    });
 });
