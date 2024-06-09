f3793224ae7d021e29a5f671b6d82caa907a89df
edg524aln <162599753+edg524aln@users.noreply.github.com>
2024-06-09 23:51:35 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 $(function() {
 
+    $('.bars').on('click', function(){
+        $('header').toggleClass('open');
+    });
+
+    $('#mask').on('click', function() {
+        $('header').toggleClass('open');
+    });
+    
+    $('.navi a').on('click', function() {
+        $('header').toggleClass('open');
+    });
+
+    $('a[href^="#"]').click(function() {
+        let href = $(this).attr("href");
+        let target = $(href == "#" || href == "" ? "html" : href);
+        let posi = target.offset().top;
+
+        $("html, body").animate({scrollTop: posi}, 600, "swing");
+        return false;
+    });
+
     $('.show-more, .modal-mask').on('click', function() {
         $('.modal').toggleClass('modal-open');
     });
